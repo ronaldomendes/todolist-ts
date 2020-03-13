52fcb4b092a010cb6f8c1138fb9140946052f7de
Treinamento <Treinamento@RD723903.raiadrogasil.com>
2020-03-13 17:36:16 -0300

    Atualizando o projeto

---
+++ src/app/newlist/newlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newlist',
+  styleUrls: ['./newlist.component.css'],
+  templateUrl: './newlist.component.html'
+})
+export class NewlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
